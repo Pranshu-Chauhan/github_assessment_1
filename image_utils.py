38d9e5fb31cfc269138ceae6ec26c7720fc7b98d
Pranshu Chauhan <pranshu26092003@gmail.com>
2026-01-14 17:52:58 +0530

    Initial image utility functions

---
+++ image_utils.py
@@ -0,0 +1,11 @@
+def read_image(path):
+    print(f"Reading image from {path}")
+
+def convert_to_grayscale(img):
+    print("Converting image to grayscale")
+
+def show_image(img):
+    print("Showing image")
+
+def save_image(img, path):
+    print(f"Saving image to {path}")
